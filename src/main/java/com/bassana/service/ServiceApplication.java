9236ad2efab1045453e3fc5e92dd2c63c7665f62
Bassana07 <moussa60783979.com>
2026-01-09 16:35:54 +0000

    config

--- src/main/java/com/bassana/service/ServiceApplication.java
+++ src/main/java/com/bassana/service/ServiceApplication.java
@@ -1,4 +1,4 @@
-package com.igit.authentification;
+package com.bassana.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
